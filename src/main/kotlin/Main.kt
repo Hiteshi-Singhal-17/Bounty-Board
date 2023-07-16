26a0df3714cde1bdfeee7f5b8cacae748e507f13
singhh22 <singhh22@medtronic.com>
2023-07-16 18:24:51 +0530

    Adding functions to make code modular.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,31 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 5
 fun main() {
     println("The hero announces her presence to the world.")
     println(HERO_NAME)
-    var playerLevel = 4
     println(playerLevel)
 
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
+    println("Time passes..")
+    println("The hero returns from her quest.")
 
-    val quest: String = when(playerLevel) {
+    playerLevel += 1
+    println(playerLevel)
+    readBountyBoard()
+}
+
+
+private fun readBountyBoard() {
+    println("The hero approaches the bounty board. It reads:")
+    println(obtainQuest(playerLevel, "paladin", true))
+}
+
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String,
+    hasAngeredBarbarians: Boolean,
+    hasBefriendedBarbarians: Boolean = false
+): String =
+    when (playerLevel) {
         1 -> "Meet Mr.Bubbles in the land of soft things."
         in 2..5 -> {
             val canTalkToBarbarian = !hasAngeredBarbarians &&
@@ -24,15 +40,7 @@ fun main() {
         8 -> "Defeat Nogartse, bringer of death and eater of worlds."
         else -> "There are no quests right now."
     }
-    println("The hero approaches the bounty board. It reads:")
-    println(quest)
 
-    println("Time passes..")
-    println("The hero returns from her quest.")
-
-    playerLevel += 1
-    println(playerLevel)
-}
 
 
 
