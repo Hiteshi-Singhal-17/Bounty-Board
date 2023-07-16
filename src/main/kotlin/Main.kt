892115bad1f862e03f8ff82dadedbf444ddefe2a
singhh22 <singhh22@medtronic.com>
2023-07-16 16:37:40 +0530

    Refactor after code analysis.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main() {
     println("The hero announces her presence to the world.")
     val heroName = "Madrigal"
     println(heroName)
-    var playerLevel: Int = 4
+    var playerLevel = 4
 
     println(playerLevel)
     println("The hero embarks on her journey to locate the enchanted sword.")
